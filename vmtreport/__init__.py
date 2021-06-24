8a92a29ebf1e7fd4577b4231095b106fed7de873
R.A. Stern <rastern@users.noreply.github.com>
2021-06-24 16:16:10 -0400

    Add extended email handler and HTML datatable formatting

--- vmtreport/__init__.py
+++ vmtreport/__init__.py
@@ -12,9 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # libraries
+import base64
+from email.message import EmailMessage
+from email.utils import COMMASPACE
+import mimetypes
+import re
+import smtplib
+
 import arbiter
 import vmtconnect as vc
-import vmtreport.util as vu
+from vmtconnect.security import Credential
+
+from vmtreport import common
 from vmtreport import stats
 from vmtreport import actions
 from vmtreport import targets
@@ -34,6 +43,25 @@ __all__ = [
     'GroupedData',
     'auth_credstore'
 ]
+
+EMAIL_HTML_HEAD = '''
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN” “https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd”>
+<html xmlns=“https://www.w3.org/1999/xhtml”>
+<head>
+<title>{SUBJECT}</title>
+<meta http–equiv=“Content-Type” content=“text/html; charset=UTF-8” />
+<meta http–equiv=“X-UA-Compatible” content=“IE=edge” />
+<meta name=“viewport” content=“width=device-width, initial-scale=1.0 “ />
+<style>
+{STYLE}
+</style>
+</head>
+<body>
+'''
+EMAIL_HTML_FOOT = '''
+</body>
+</html>
+'''
 
 
 
@@ -141,6 +169,12 @@ class ActionData(Connection):
 
 class TargetData(Connection):
     """
+    Data handler for pulling Turbonomic Target infomration.
+
+    Arguments:
+        config (dict): Dictionary of handler configuration data
+        **kwargs: Additional handler specific options. These will override any
+            in the `config` options.
     """
     def __init__(self, config, **kwargs):
         super().__init__(config, **kwargs)
@@ -152,12 +186,214 @@ class TargetData(Connection):
         Standard Arbiter interface implementation. Returns the grouped and sorted
         data report based on the config.
         """
-        return actions.TargetDataReport(self._connection, self.options).apply()
+        return targets.TargetDataReport(self._connection, self.options).apply()
+
+
+class DataResource(arbiter.handlers.FileHandler):
+    """
+    Output data handler for preparing data for use as the email message body.
+    Requires use of a compatible Notification handler, as this handler does not
+    write to a file.
+
+    Arguments:
+        config (dict): Dictionary of handler configuration data
+        **kwargs: Additional handler specific options. These will override any
+            in the `config` options.
+    """
+    __slots__ = ['filename']
+
+    def __init__(self, config, **kwargs):
+        config['resource'] = ''
+        super().__init__(config, **kwargs)
+
+        self.filename = None
+        self.mimetype = self.options.get('type', 'text/plain')
+        self.encoding = self.options.get('encoding', 'base64')
+
+    def enc_txt(self):
+        if self.encoding:
+            return f";{self.encoding}"
+        else:
+            return ''
+
+    def encode(self, data):
+        if self.encoding == 'base64':
+            return base64.b64encode(data.encode())
+        elif self.encoding:
+            return data.encode(encoding=self.encoding)
+        else:
+            return data
+
+    def set(self, data):
+        """
+        Stores the conventional output in the 'filename' reference instead
+        of writing to file.
+        """
+        if not data:
+            pass
+
+        if 'fieldnames' not in self.options:
+            self.options['fieldnames'] = data[0].keys()
+
+        data = self.encode(common.format(
+                    self.options['format'].get('type', 'tabulate'),
+                    data,
+                    self.options['fieldnames'],
+                    self.options.get('format')
+                    )).decode()
+
+        self.filename = f"data:{self.mimetype}{self.enc_txt()},{data}"
+
+    def atexit(self):
+        pass
+
+
+class ExtendedEmailHandler(arbiter.handlers.EmailHandler):
+    """
+    An extension to the Arbiter EmailHandler. Adds HTML capability as well as
+    support for data: URI resource definitions.
+    """
+    def __init__(self, config, **kwargs):
+        super().__init__(config, **kwargs)
+
+        self._emailheaders.append('html_prologue')
+        self._emailheaders.append('html_epilogue')
+
+    @staticmethod
+    def get_data_type(resource):
+        try:
+            if resource.startswith('data:'):
+                m = re.search('(?:data:)([^;]+)(?:;(?:[\S]+))?,(?:.*)', resource)
+
+                return m[1]
+        except AttributeError:
+            pass
+
+        return resource
+
+    @staticmethod
+    def any_html(res_list):
+        for res in res_list:
+            if ExtendedEmailHandler.get_data_type(res) == 'text/html':
+                return True
+
+        return False
+
+    @staticmethod
+    def decode(resource):
+        try:
+            if resource.startswith('data:'):
+                # 1 - type, 2 - encoding, 3 - data
+                m = re.search('(?:data:)([^;]+)(?:;([\S]+))?,(.*)', resource)
+
+                if m[2] == 'base64':
+                    return base64.b64decode(m[3]).decode()
+                elif m[2]:
+                    return m[3].decode(encoding=m[2])
+                else:
+                    return m[3]
+        except AttributeError:
+            pass
+
+        return resource
+
+    @staticmethod
+    def get_body(body, resources):
+        _data = ''
+
+        for res in resources:
+            _data += ExtendedEmailHandler.decode(res)
+
+        return arbiter.parse_string(body, data=_data)
+
+    @staticmethod
+    def set_headers(msg, options, headers):
+        for k in options:
+            if k in headers:
+                if isinstance(options[k], list):
+                    msg[k] = arbiter.parse_string(COMMASPACE.join(options[k]))
+                else:
+                    msg[k] = arbiter.parse_string(options[k])
+
+        return msg
+
+    @staticmethod
+    def attach_files(msg, files):
+        for file in files:
+            ctype, encoding = mimetypes.guess_type(file)
+
+            # unknown, treat as binary
+            if ctype is None or encoding is not None:
+                ctype = 'application/octet-stream'
+
+            maintype, subtype = ctype.split('/', 1)
+
+            with open(file, 'rb') as fp:
+                msg.add_attachment(fp.read(),
+                               maintype=maintype,
+                               subtype=subtype,
+                               filename=os.path.basename(file))
+
+        return msg
+
+    def send(self):
+        msg = EmailMessage()
+        msg = self.set_headers(msg, self.options['email'], self._emailheaders)
+
+        if self.errors:
+            error_msg = '\n\n'.join([str(x) for x in self.errors])
+            body = self.options['email'].get('body_error', self.default_body_error)
+            msg.set_content(arbiter.parse_string(body, errors=error_msg))
+        else:
+            data = []
+            files = [x for x in self.files if x and not x.startswith('data:')]
+
+            if self.options.get('embed_data_resource', True):
+                data = [x for x in self.files if x and x.startswith('data:')]
+
+            subs = {
+                'STYLE': self.options.get('style', ''),
+                'SUBJECT': arbiter.parse_string(self.options['email']['subject'])
+            }
+            body = self.get_body(self.options['email']['body'], data)
+
+            if self.any_html(data) and self.options.get('html', False):
+                p = self.options['email'].get('html_prologue',
+                            arbiter.parse_string(EMAIL_HTML_HEAD, **subs))
+                e = self.options['email'].get('html_epilogue', EMAIL_HTML_FOOT)
+                msg.set_content(f"{p}{body}{e}", subtype='html')
+            else:
+                msg.set_content(body)
+
+            if files:
+                msg = self.attach_files(msg, files)
+
+        if self.options['smtp'].get('ssl', False):
+            klass = smtplib.SMTP_SSL
+        elif self.options['smtp'].get('lmtp', False):
+            klass = smtplib.LMTP
+        else:
+            klass = smtplib.SMTP
+
+        _smtp_opts = self._EmailHandler__smtp_options()
+
+        with klass(host=self.options['smtp']['host'], **_smtp_opts) as smtp:
+            if self.options['smtp'].get('tls', False):
+                tlsargs = {
+                    x: self.options['smtp'][x] for x in self.options['smtp']
+                        if x in ['keyfile', 'certfile']
+                }
+                smtp.starttls(**tlsargs)
+
+            if self.options['smtp'].get('username', None) \
+            and self.options['smtp'].get('password', None):
+                smtp.login(self.options['smtp']['username'], self.options['smtp']['password'])
+
+            smtp.send_message(msg)
+
 
 
 def auth_credstore(obj):
-    from vmtconnect.security import Credential
-
     return {'auth': Credential(obj['keyfile'], obj['credential']).decrypt()}
 
 
@@ -165,5 +401,8 @@ def auth_credstore(obj):
 arbiter.HANDLERS.register('vmtconnect', Connection)
 arbiter.HANDLERS.register('vmtgroupeddata', GroupedData)
 arbiter.HANDLERS.register('vmtactiondata', ActionData)
-#arbiter.HANDLERS.register('vmtactiondata', TargetData)
+arbiter.HANDLERS.register('vmttargetdata', TargetData)
+arbiter.HANDLERS.register('dataresource', DataResource)
+arbiter.HANDLERS.register('extendedemail', ExtendedEmailHandler)
+
 arbiter.AUTH.register('credstore', auth_credstore)
--- vmtreport/common.py
+++ vmtreport/common.py
@@ -12,9 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # libraries
+from dataclasses import dataclass
 from enum import Enum
 import re
 
+import vmtreport.util as vu
+
+
+
+@dataclass
+class HTML:
+    TABLE: str = 'table'
+    TBODY: str = 'tbody'
+    TD: str = 'td'
+    TH: str = 'th'
+    THEAD: str = 'thead'
+    TR: str = 'tr'
 
 
 class Options(Enum):
@@ -24,6 +37,8 @@ class Options(Enum):
     FILTERS = 'filters'
     #:
     GROUPS = 'groups'
+    #:
+    RESPONSE_FILTER = 'response_filter'
     #:
     SCOPES = 'scopes'
     #:
@@ -118,3 +133,109 @@ class DataField:
             return self.tree_get(data[keys[0]], keys[1])
 
         return data[keys[0]]
+
+
+
+def html_attr(attr, v, delim=' '):
+    if v:
+        v = [v] if isinstance(v, str) else [x for x in v if x]
+        return f" {attr}=\"{delim.join(v)}\"" if v else ''
+
+    return ''
+
+
+def html_tag(tag, data, classes=None, styles=None):
+    attr = ''
+    attr += html_attr('class', classes)
+    attr += html_attr('style', styles, '; ')
+
+    return f"<{tag}{attr}>{data}</{tag}>"
+
+
+# a replacement for tabulate's function
+def _html_row(tag, unsafe, css_cls, css_style, cell_values, colwidths, colaligns):
+    try:
+        from html import escape as htmlescape
+    except ImportError:
+        from cgi import escape as htmlescape
+
+    if not css_cls:
+        css_cls = {}
+
+    if not css_style:
+        css_style = {}
+
+    cols = []
+    typ = 'header_cols' if tag == HTML.TH else 'cols'
+
+    for i, v in enumerate(cell_values):
+        v = v if unsafe else htmlescape(v)
+
+        cols.append(html_tag(tag, v.strip(),
+                             css_cls.get(typ, [None]*(i+1))[i],
+                             css_style.get(typ, [None]*(i+1))[i]))
+
+    row = html_tag(HTML.TR, ''.join(cols).strip(),
+                   css_cls.get('row'),
+                   css_style.get('row'))
+
+    if tag == HTML.TH:
+        return f"<{HTML.TABLE}>\n<{HTML.THEAD}>\n{row}\n</{HTML.THEAD}>\n<{HTML.TBODY}>"
+    else:
+        return row
+
+
+def format(type, data, fields, options=None):
+    try:
+        _func = _formatters[type.lower()]
+
+        return _func(data, fields, options)
+    except KeyError:
+        raise NameError(f"Unknown format type: {type}")
+
+
+def fmt_tabulate(data, fields, options=None):
+    from tabulate import tabulate
+
+    kwargs = {'headers': fields}
+
+    if options:
+        kwargs.update({x: options[x] for x in options
+                            if x not in ['type']
+                      })
+
+    return tabulate([[r[_] for _ in r] for r in data], **kwargs)
+
+
+def fmt_htmltable(data, fields, options=None):
+    from functools import partial
+    import tabulate
+
+    # tabulate uses namedtuples, so we're gonna monkeypatch it
+    # tabulate should probably use a dataclass
+    # (tag, unsafe, styles, values, colwidths, colaligns)
+    fmt = tabulate._table_formats['html']
+    fmt = fmt._replace(headerrow = partial(_html_row, HTML.TH,
+                                           not options.get('htmlsafe', True),
+                                           options.get('classes'),
+                                           options.get('styles')))
+    fmt = fmt._replace(datarow = partial(_html_row, HTML.TD,
+                                         not options.get('htmlsafe', True),
+                                         options.get('classes'),
+                                         options.get('styles')))
+    tabulate._table_formats['html'] = fmt
+    kwargs = {'headers': fields, 'tablefmt': 'html'}
+
+    if options:
+        kwargs.update({x: options[x] for x in options
+                            if x not in ['type', 'htmlsafe', 'classes', 'styles']
+                      })
+
+    return tabulate.tabulate([[r[_] for _ in r] for r in data], **kwargs)
+
+
+
+_formatters = {
+    'tabulate': fmt_tabulate,
+    'htmltable': fmt_htmltable
+}
